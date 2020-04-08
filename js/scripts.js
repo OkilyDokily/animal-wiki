0f85741844d308129d293c039d1c901e21b4ab9f
me <me@me.me.com>
2020-04-08 03:59:38 -0700

    create event that responds depending on which img you click on

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".animal-grid").click(function(e){
+    
+    if(e.target.id === "giraffe"){
+     
+      $(".animal-grid").animate({
+        
+      })
+    }
+    else if (event.target.id === "honeybee"){
+
+    }
+    else if (event.target.id == "coati"){
+
+    }
+  })
+    
+  
+
+});
